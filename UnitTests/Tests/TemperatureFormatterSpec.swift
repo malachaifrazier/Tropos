41680d8171d03dec443b28d75758b83dc63a6c8f
Adam Sharp <adsharp@me.com>
2016-04-18 14:15:50 -0400

    Port TemperatureFormatter to swift and add a spec

---
+++ UnitTests/Tests/TemperatureFormatterSpec.swift
@@ -0,0 +1,26 @@
+@testable import Tropos
+import Quick
+import Nimble
+
+final class TemperatureFormatterSpec: QuickSpec {
+    override func spec() {
+        it("can format fahrenheit temperatures in celsius") {
+            let formatter = TemperatureFormatter(unitSystem: .Metric)
+            let temperature = Temperature(fahrenheitValue: 32)
+            expect(formatter.stringFromTemperature(temperature)) == "0°"
+        }
+
+        it("can format celsius temperatures in fahrenheit") {
+            let formatter = TemperatureFormatter(unitSystem: .Imperial)
+            let temperature = Temperature(celsiusValue: 0)
+            expect(formatter.stringFromTemperature(temperature)) == "32°"
+        }
+
+        it("uses the default unit system in settings if not specified") {
+            SettingsController().unitSystem = .Metric
+            let formatter = TemperatureFormatter()
+            let temperature = Temperature(fahrenheitValue: 32)
+            expect(formatter.stringFromTemperature(temperature)) == "0°"
+        }
+    }
+}
